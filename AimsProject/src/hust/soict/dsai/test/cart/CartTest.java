59e8699e9b989e48fb70e620ca022a34a38a675d
Giang Hoang <trinhhoanggiang2911@gmail.com>
2023-05-30 21:32:36 +0700

    ...

--- AimsProject/src/hust/soict/dsai/test/cart/CartTest.java
+++ AimsProject/src/hust/soict/dsai/test/cart/CartTest.java
@@ -6,25 +6,34 @@ import hust.soict.dsai.aims.disc.DigitalVideoDisc;
 public class CartTest {
 
 	public static void main(String[] args) {
-		Cart cart = new Cart();
+		Cart cart1 = new Cart();
 		
 		DigitalVideoDisc dvd1= new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-		cart.addDigitalVideoDisc(dvd1);
+		cart1.addDigitalVideoDisc(dvd1);
 		
 		DigitalVideoDisc dvd2= new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
-		cart.addDigitalVideoDisc(dvd2);
+		cart1.addDigitalVideoDisc(dvd2);
 		
 		DigitalVideoDisc dvd3= new DigitalVideoDisc("Aladin", "Animation", 18.99f);
-		cart.addDigitalVideoDisc(dvd3);
+		cart1.addDigitalVideoDisc(dvd3);
 		
+		//		Test add
+//		Array Parameter
+		Cart cart2 = new Cart();
+		DigitalVideoDisc dvdList[] = {dvd1,dvd2,dvd3};
+		cart2.addDigitalVideoDisc(dvdList);
+		
+//		Arbitrary Parameter
+		Cart cart3 = new Cart();
+		cart3.addDigitalVideoDisc(dvd1,dvd2,dvd3);
 		// Test for print 
-		cart.print();
+		cart1.print();
 		
 		// Test for search
-		cart.search(3);
-		cart.search("The Lion King");
-		cart.search(10);
-		cart.search("Spider Man");
+		cart1.search(3);
+		cart1.search("The Lion King");
+		cart1.search(10);
+		cart1.search("Spider Man");
 	}
 
 }
