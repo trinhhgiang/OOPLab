e9c1b5a3cdcb229888e9b8b4f7912ffb7764aed5
Giang Hoang <trinhhoanggiang2911@gmail.com>
2023-06-11 12:00:17 +0700

    ...

---
+++ AimsProject/src/hust/soict/dsai/aims/media/Track.java
@@ -0,0 +1,29 @@
+package hust.soict.dsai.aims.media;
+
+public class Track implements Playable {
+	private String title;
+	private int length;
+	public Track(String title, int length) {
+		this.title=title;
+		this.length=length;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public int getLength() {
+		return length;
+	}
+	public void play() {
+		System.out.println("Playing Track: " + this.getTitle());
+		System.out.println("Track length: " + this.getLength());
+	}
+	public boolean equals(Object o) {
+		if (o instanceof Track) {
+			Track other = (Track) o;
+			if (this.title.equals(other.getTitle()) &&(this.length==other.getLength())){
+				return true;
+			}
+		}
+		return false;
+	}
+}
